6e73f9968235d0210de87fc947021d3fbb27d609
SiHyeon Bae <devbsh013@gmail.com>
2022-07-12 14:11:10 +0900

    0712 hack 언어 공부

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -1,14 +1,15 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Fill.asm
+    (LOOP)
+    @24576
+    D=M
+    @8
+    D;JEQ//If D=0 goto 0
+    @SCREEN
+    M=-1
+    @LOOP
+    0;JMP 
+    @SCREEN
+    M=0
+    @LOOP
+    0;JMP 
 
-// Runs an infinite loop that listens to the keyboard input.
-// When a key is pressed (any key), the program blackens the screen,
-// i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
-// When no key is pressed, the program clears the screen, i.e. writes
-// "white" in every pixel;
-// the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
